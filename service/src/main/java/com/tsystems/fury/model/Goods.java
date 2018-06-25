e0b48407badceb18cc70eeed931b3d73afecaeb4
Valentin <wippi2010@rambler.ru>
2018-06-25 17:39:53 +0300

    +integration

--- service/src/main/java/com/tsystems/fury/model/Goods.java
+++ service/src/main/java/com/tsystems/fury/model/Goods.java
@@ -1,10 +1,12 @@
 package com.tsystems.fury.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@AllArgsConstructor
 public class Goods implements Serializable {
 
     private static final long serialVersionUID = 3322186557067294220L;
--- service/src/main/java/com/tsystems/fury/model/GoodsInfo.java
+++ service/src/main/java/com/tsystems/fury/model/GoodsInfo.java
@@ -1,10 +1,12 @@
 package com.tsystems.fury.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@AllArgsConstructor
 public class GoodsInfo implements Serializable{
 
     private static final long serialVersionUID = -3147943338478953248L;
